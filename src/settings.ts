177ba9c0fc21b26be543acef20d6960c6f76b459
Albert Lee <leeeboo@users.noreply.github.com>
2021-07-12 14:19:34 +0800

    init

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+export const PLATFORM_NAME = 'XiaoMiAirConditionerMT6';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-mi-air-conditioner-mt6-co2';
